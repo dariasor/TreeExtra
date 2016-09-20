493983c105f62403a5fad685d70dd11d8ed3e16a
Daria Sorokina <fisharik@gmail.com>
2016-09-19 19:09:46 -0700

    Save logs from old experiments into log.archive.txt

--- shared/LogStream.cpp
+++ shared/LogStream.cpp
@@ -5,13 +5,25 @@
 #include "LogStream.h"
 #include "definitions.h"
 
-//static variable
+//static variable showing if log messages are sent into the standard output in addition to log files
 bool LogStream::doOut = true;
 
 //static initialization, needs to be called once in the whole program
 void LogStream::init(bool doOut_in)
 { 
 	doOut = doOut_in;
+
+	//if log.txt exists, append its content to log.archive.txt
+	fstream foldlog;
+	foldlog.open("log.txt", ios_base::in);
+	if(foldlog)
+	{
+		fstream farchive;
+		farchive.open("log.archive.txt", ios_base::out | ios_base::app);
+		farchive << foldlog.rdbuf();
+	}
+	
+	//open clean log.txt
 	fstream fout; 
 	fout.open("log.txt", ios_base::out);
 	if(!fout)
