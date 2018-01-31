27bd9ddfc25a5df46e9c281ddade61721a1510c2
dariasor <fisharik@gmail.com>
2018-01-31 10:47:09 -0800

    minor cleanup

--- OctavePlots/plot_effects.m
+++ OctavePlots/plot_effects.m
@@ -1,12 +1,9 @@
-% (C)Alexander Sorokin,  Daria Sorokina, 2009
-% License: New BSD.
-
 folder='./';
 
 files=dir([folder '*effect.txt']);
 for iF = 1:numel(files)
 
-    fn = files(iF).name; %'x1.pdf.txt';
+    fn = files(iF).name; 
 
     data = dlmread([folder fn],'\t',2,0);
 
@@ -17,7 +14,4 @@ for iF = 1:numel(files)
     make_effect_plot(xvalues, xcounts, values, fn)
 
     print(gcf,'-depsc',[folder fn '.eps']);
-    %print(gcf,'-djpeg90',[folder fn '.jpg']);
-
-    %pause
 end 
